198afd66a0f9e41a0f12069a13d1118fc8f7771a
mosmo46 <73312834+mosmo46@users.noreply.github.com>
2022-05-19 12:55:27 +0300

    JSONSerializationTests

--- TestAutomationCourse/Demos/d04.Serialization/JSONSerializationTests.cs
+++ TestAutomationCourse/Demos/d04.Serialization/JSONSerializationTests.cs
@@ -7,25 +7,61 @@ namespace TestAutomationCourse.Demos.d04.Serialization
     [TestFixture]
     internal class JSONSerializationTests
     {
+           public string json = File.ReadAllText(".//Exercises//e05.JSON//Beatles.json");
+           public Album album;
+      [SetUp]
+        public void Setup()
+        {
+            album = JsonConvert.DeserializeObject<Album>(json);
+        }
+
         [Test]
         public void There_are_four_artists()
         {
-            string json = File.ReadAllText(".//Exercises//e05.JSON//Beatles.json");
-            Album album = JsonConvert.DeserializeObject<Album>
-                (json);
             Assert.That(album.beatles.artists.Length, Is.EqualTo(4));
         }
 
         [Test]
         public void Two_are_dead_and_two_are_alive()
         {
+            var count = album.beatles.artists.Length;
+            var alive = 0;
+            var dead = 0;
+            for (int i = 0; i < count; i++)
+            {
+            var isAlive = album.beatles.artists[i].isAlive;
+            if (isAlive.Equals("Yes"))
+            {
+                alive++;
+            }
+            else
+            {
+                dead++;
+            }
+            }
 
+            Assert.That(alive, Is.EqualTo(2));
+            Assert.That(dead, Is.EqualTo(2));
         }
 
         [Test]
         public void Ringo_plays_drums()
         {
 
+            var count = album.beatles.artists.Length;
+            string plays = "";
+            for (int i = 0; i < count; i++)
+            {
+                var name = album.beatles.artists[i].name;
+
+                if (name.Equals("Ringo Starr"))
+                {
+                     plays = album.beatles.artists[i].plays;
+                }
+
+            }
+            Assert.That(count, Is.EqualTo(4));
+            Assert.That(plays, Is.EqualTo("Drums"));
         }
 
 
--- TestAutomationCourse/Demos/d04.Serialization/XMLSerializationTests.cs
+++ TestAutomationCourse/Demos/d04.Serialization/XMLSerializationTests.cs
@@ -8,31 +8,36 @@ namespace TestAutomationCourse.Demos.d04.Serialization
     [TestFixture]
     internal class XMLSerializationTests
     {
-        [Test]
-        public void First_name_is_joe()
+
+          public XmlSerializer serializer =new XmlSerializer(typeof(Person));
+
+           public Person person;
+
+        [SetUp]
+        public void Setup()
         {
-            XmlSerializer serializer =
-                 new XmlSerializer(typeof(Person));
-
-            Person person;
-
             using (TextReader reader = new StringReader(XMLExamples.GetPersonXML()))
             {
                 person = (Person)serializer.Deserialize(reader);
             }
+        }
+
+        [Test]
+        public void First_name_is_joe()
+        {
             Assert.That(person.FirstName, Is.EqualTo("Joe"));
         }
 
         [Test]
         public void Joe_has_three_children()
         {
-
+            Assert.That(person.Children.Child.Count, Is.EqualTo(3));
         }
 
         [Test]
         public void Second_child_name_is_jim()
         {
-
+            Assert.That(person.Children.Child[1].name, Is.EqualTo("Jim"));
         }
     }
 
